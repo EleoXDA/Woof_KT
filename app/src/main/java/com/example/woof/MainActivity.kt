3682bb24b3e85d1e6d278053cec19aa72314fcb5
Elbay Malik <27622683+EleoXDA@users.noreply.github.com>
2022-08-08 21:53:14 +0200

    Add elevation to cards for more realistic look

--- app/src/main/java/com/example/woof/MainActivity.kt
+++ app/src/main/java/com/example/woof/MainActivity.kt
@@ -81,7 +81,8 @@ fun WoofApp() {
 @Composable
 fun DogItem(dog: Dog, modifier: Modifier = Modifier) {
     Card(
-        modifier = modifier.padding(8.dp)
+        modifier = modifier.padding(8.dp),
+        elevation = 4.dp
     ) {
         Row(
             modifier = Modifier
