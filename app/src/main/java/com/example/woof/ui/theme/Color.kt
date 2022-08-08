386d06ee1716edb178dbf1864d7d0c44aab6f4c8
Elbay Malik <27622683+EleoXDA@users.noreply.github.com>
2022-08-08 21:06:43 +0200

    Add colors for dark theme

--- app/src/main/java/com/example/woof/ui/theme/Color.kt
+++ app/src/main/java/com/example/woof/ui/theme/Color.kt
@@ -28,3 +28,9 @@ val Grey900 = Color(0xFF202124)
 val Grey700 = Color(0xFF5F6368)
 val Green50 = Color(0xFFE6F4EA)
 val Green100 = Color(0xFFCEEAD6)
+
+//Dark Theme
+val White = Color(0xFFFFFFFF)
+val Grey100 = Color(0xFFF1F3F4)
+val Cyan900 = Color(0xFF007B83)
+val Cyan700 = Color(0xFF129EAF)
